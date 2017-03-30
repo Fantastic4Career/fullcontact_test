b40e4276527002583a6a5b563167f165a82353b9
Shengtao Li <sevenlist0110@gmail.com>
2017-03-29 22:53:48 -0700

    [update] front end and backend for query full contact api and display response to front end

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 var request = require('request');
+var app = express();
+app.use(bodyParser());
+app.use(express.static('public'));
 
-console.log("hi>>>");
-request({
-  method: 'GET',
-  uri: 'https://api.fullcontact.com/v2/company/search.json',
-  qs: {companyName: 'eatsa'},
-  headers: {
-    'X-FullContact-APIKey': '957f1beac769ac2c'
-  },
-}, 
-function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    console.log("response is>>>", info);
-  }
+
+app.post('/api/fullcontact', function (req, res) {
+  var companyName = req.body.companyName;
+  console.log("hi>>>");
+  console.log("request body is >>>", req.body);
+  console.log("companyName is >>>", companyName);
+  request({
+    method: 'GET',
+    uri: 'https://api.fullcontact.com/v2/company/search.json',
+    qs: {companyName: companyName},
+    headers: {
+      'X-FullContact-APIKey': '957f1beac769ac2c'
+    },
+  }, 
+  function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      console.log("response is>>>", info);
+      res.json(info);
+    }
+  });
+  
+});
+
+app.get('/', function(req,res) {
+  res.sendfile('public/index.html');
+});
+
+app.get('/*', function(req, res){
+  res.redirect('/');
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!')
 });
